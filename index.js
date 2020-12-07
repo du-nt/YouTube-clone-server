cdee13838bf675e330e7cdfdd698705b7f331615
yaokaoya <mahoagray@gmai.com>
2020-12-07 16:29:09 +0900

    update

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const { AwakeHeroku } = require("awake-heroku");
+const herokuAwake = require("heroku-awake");
 const cors = require("cors");
 const app = express();
 
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 
-AwakeHeroku.add({
-  url: "https://youtube-clone-client.herokuapp.com/",
-});
+const url = "https://youtube-clone-client.herokuapp.com/";
+const time = 10;
 // app.use(cors());
 app.use(
   cors({
@@ -40,4 +39,7 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/video", require("./routes/video"));
 app.use("/api/auth", require("./routes/auth"));
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(port, () => {
+  herokuAwake(url, time);
+  console.log(`App listening on port ${port}!`);
+});
