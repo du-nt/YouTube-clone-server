565c58c19f2665136b56d77d8f32db0f0ec509b6
du.nt <dunt@btc-studio.io>
2023-08-19 19:04:05 +0700

    updated front end domain

--- config/passport.js
+++ config/passport.js
@@ -61,7 +61,7 @@ const googleStrategyConfig = new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://api-youtubeclone.herokuapp.com/auth/google/callback",
+    callbackURL: "https://youtube-clone-server-wek8.onrender.com/auth/google/callback",
     // callbackURL: "http://localhost:8000/auth/google/callback",
   },
   (req, accessToken, refreshToken, profile, done) => {
